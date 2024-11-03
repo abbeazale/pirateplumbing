e609978553ad7ac2e6004c98efe0e77f1210da4d
Abbe Azale <abbeazale98@gmail.com>
2024-11-03 13:46:01 -0800

    added meta to documents file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta property="og:image" content="/pirate.svg" />
+        <meta name="keywords" content="vancouver plumbing, emergency plumbing, plumbing repair, residential plumbing, commercial plumbing, 24/7 plumbing, same day plumbing, weekend plumber, drain repair, pipe repair, leak detection, water heater installation, water heater repair, sewer line repair, sewer repair, clogged drain, blocked toilet, leaking pipe, running toilet, low water pressure, water leak, burst pipe, sewer backup, hot water problem, bathroom plumbing, kitchen plumbing, noisy pipes, plumber near me, local plumber, best plumber vancouver" />
+        <link rel="icon" href="https://pirateplumbing.ca/pirate.svg" />
+        <meta property="og:title" content="Pirate Plumbing" />
+        <meta property="og:description" content="We offer 24-hour, seven days a week plumbing services for the Greater Vancouver Area that is affordable and professional. Contact us today to experience the difference." />
+        <meta property="og:image" content="/pirate.svg" />
+        <meta property="og:url" content="https://pirateplumbing.ca" />
+
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
