c3936664fcaaf366ebd501df720c7bfceb66fc87
Abbe Azale <abbeazale98@gmail.com>
2024-11-03 11:11:36 -0800

    set an image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -101,7 +101,7 @@ export default function Home() {
       <Head>
         <title>Pirate Plumbing</title>
         <meta name="keywords" content="vancouver plumbing, emergency plumbing, plumbing repair, residential plumbing, commercial plumbing, 24/7 plumbing, same day plumbing, weekend plumber, drain repair, pipe repair, leak detection, water heater installation, water heater repair, sewer line repair, sewer repair, clogged drain, blocked toilet, leaking pipe, running toilet, low water pressure, water leak, burst pipe, sewer backup, hot water problem, bathroom plumbing, kitchen plumbing, noisy pipes, plumber near me, local plumber, best plumber vancouver" />
-        <link rel="icon" href="pirate.svg" />
+        <link rel="icon" href="https://pirateplumbing.ca/pirate.svg" />
         <meta property="og:title" content="Pirate Plumbing" />
         <meta property="og:description" content="We offer 24-hour, seven days a week plumbing services for the Greater Vancouver Area that is affordable and professional. Contact us today to experience the difference." />
         <meta property="og:image" content="/pirate.svg" />
