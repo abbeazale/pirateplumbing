ec6e65a504bf90c7ac8b2cb1c5943b6178d0bb07
Abbe Azale <abbeazale98@gmail.com>
2024-11-04 21:34:49 -0800

    added twitter cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -101,11 +101,15 @@ export default function Home() {
       <Head>
         <title>Pirate Plumbing</title>
         <meta name="keywords" content="vancouver plumbing, emergency plumbing, plumbing repair, residential plumbing, commercial plumbing, 24/7 plumbing, same day plumbing, weekend plumber, drain repair, pipe repair, leak detection, water heater installation, water heater repair, sewer line repair, sewer repair, clogged drain, blocked toilet, leaking pipe, running toilet, low water pressure, water leak, burst pipe, sewer backup, hot water problem, bathroom plumbing, kitchen plumbing, noisy pipes, plumber near me, local plumber, best plumber vancouver" />
-        <link rel="icon" href="/gicon.ico" />
+        <link rel="icon" href="https://www.pirateplumbing.ca/gicon.ico" />
         <meta property="og:title" content="Pirate Plumbing" />
         <meta property="og:description" content="We offer 24-hour, seven days a week plumbing services for the Greater Vancouver Area that is affordable and professional. Contact us today to experience the difference." />
-        <meta property="og:image" content="/gicon.ico" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="Pirate Plumbing" />
+        <meta property="twitter:description" content="We offer 24-hour, seven days a week plumbing services for the Greater Vancouver Area that is affordable and professional. Contact us today to experience the difference." />
+        <meta property="og:image" content="https://www.pirateplumbing.ca/gicon.ico" />
         <meta property="description" content="We offer 24-hour, seven days a week plumbing services for the Greater Vancouver Area that is affordable and professional. Contact us today to experience the difference." />
+        <meta property="twitter:image" content="https://www.pirateplumbing.ca/pirate.svg" />
         <meta property="og:url" content="https://pirateplumbing.ca" />
         <meta name="google-site-verification" content="J0L-DHJdzVbRcfqyQL3t5k7lIHJdCc8DIxcll8eswws" />
         <meta property="og:site_name" content="Pirate Plumbing" />
